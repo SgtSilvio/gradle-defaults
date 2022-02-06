d9d7cf0d8582c9e4052bb373cedc96916352e133
Silvio Giebl <gieblsilvio@outlook.com>
2022-02-06 13:09:27 +0100

    Remove configureIdeaTestSources as it will be added to Gradle 7.4

--- src/main/kotlin/com/github/sgtsilvio/gradle/defaults/DefaultsPlugin.kt
+++ src/main/kotlin/com/github/sgtsilvio/gradle/defaults/DefaultsPlugin.kt
@@ -2,17 +2,12 @@ package com.github.sgtsilvio.gradle.defaults
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.jvm.JvmTestSuite
 import org.gradle.api.tasks.bundling.AbstractArchiveTask
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.javadoc.Javadoc
 import org.gradle.api.tasks.testing.Test
 import org.gradle.external.javadoc.StandardJavadocDocletOptions
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.withType
-import org.gradle.plugins.ide.idea.model.IdeaModel
-import org.gradle.testing.base.TestingExtension
 
 /**
  * @author Silvio Giebl
@@ -23,11 +18,6 @@ class DefaultsPlugin : Plugin<Project> {
         configureUtf8(project)
         configureReproducibleArtifacts(project)
         configureGranularTestReports(project)
-        project.plugins.withId("org.gradle.idea") {
-            project.plugins.withId("org.gradle.jvm-test-suite") {
-                configureIdeaTestSources(project)
-            }
-        }
     }
 
     private fun configureUtf8(project: Project) {
@@ -54,18 +44,6 @@ class DefaultsPlugin : Plugin<Project> {
         project.tasks.withType<Test>().configureEach {
             reports {
                 junitXml.isOutputPerTestCase = true
-            }
-        }
-    }
-
-    private fun configureIdeaTestSources(project: Project) {
-        val ideaModel = project.extensions.getByType<IdeaModel>()
-        project.extensions.configure<TestingExtension> {
-            suites.withType<JvmTestSuite>().configureEach {
-                ideaModel.module {
-                    testSourceDirs = testSourceDirs + sources.java.srcDirs
-                    testResourceDirs = testResourceDirs + sources.resources.srcDirs
-                }
             }
         }
     }
